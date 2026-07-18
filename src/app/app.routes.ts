d6f2b1f93352a36763db1cdf750a9b231211986e
sagar-shivappa <shivappasagar@gmail.com>
2026-07-18 15:52:29 +0530

    Remove sample page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { sample } from '../shared/sample/sample';
 import { Home } from '../shared/home/home';
 import { AddProductComponent } from '../features/add-product-component/add-product-component';
 import { BillingEntryComponent } from '../features/billing-entry-component/billing-entry-component';
@@ -11,10 +10,7 @@ export const routes: Routes = [
     component: Home,
     children: [
       { path: '', component: BillingEntryComponent },
-      {
-        path: 'sample',
-        component: sample,
-      },
+
       {
         path: 'products',
         component: AddProductComponent,
